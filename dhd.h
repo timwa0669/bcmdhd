timwa0669/bcmdhd

commit 06da1feab031780c9779c054feee8ec7e306ef1d

dhd: hardcoded VENDOR_PATH, preparing for hardcoding firmware/nvram/clm_blob paths

dhd.h
@@ -2628,6 +2628,12 @@ extern uint dhd_pktgen_len;
 extern char fw_path2[MOD_PARAM_PATHLEN];
 #endif // endif
 
+#if (ANDROID_PLATFORM_VERSION < 7)
+#define DHD_LEGACY_FILE_PATH
+#endif /* ANDROID_PLATFORM_VERSION < 7 */
+#define VENDOR_PATH ""
+
+#if (0)
 #if defined(ANDROID_PLATFORM_VERSION)
 #if (ANDROID_PLATFORM_VERSION < 7)
 #define DHD_LEGACY_FILE_PATH
@@ -2640,6 +2646,7 @@ extern char fw_path2[MOD_PARAM_PATHLEN];
 #else
 #define VENDOR_PATH ""
 #endif /* ANDROID_PLATFORM_VERSION */
+#endif
 
 #if defined(ANDROID_PLATFORM_VERSION)
 #if (ANDROID_PLATFORM_VERSION < 9)

commit 227e07531802a832589d3532227a209fba4995cc

dhd: fix compilation error caused by 06da1fe

dhd.h
@@ -2628,9 +2628,11 @@ extern uint dhd_pktgen_len;
 extern char fw_path2[MOD_PARAM_PATHLEN];
 #endif // endif
 
+#if defined(ANDROID_PLATFORM_VERSION)
 #if (ANDROID_PLATFORM_VERSION < 7)
 #define DHD_LEGACY_FILE_PATH
 #endif /* ANDROID_PLATFORM_VERSION < 7 */
+#endif /* ANDROID_PLATFORM_VERSION */
 #define VENDOR_PATH ""
 
 #if (0)